969fd66c38a7e1b60e4ca3c45b647838f7cd0643
salmanzafar949 <salmanzafar949@gmail.com>
2017-08-27 03:44:48 +0500

    Added Functionality to Search books

--- LibraryManagmentSystem/view_books.Designer.cs
+++ LibraryManagmentSystem/view_books.Designer.cs
@@ -29,34 +29,61 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(179, 40);
+            this.dataGridView1.Location = new System.Drawing.Point(182, 55);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(633, 377);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Location = new System.Drawing.Point(182, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // view_books
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 456);
+            this.ClientSize = new System.Drawing.Size(956, 474);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "view_books";
             this.Text = "view_books";
             this.Load += new System.EventHandler(this.view_books_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- LibraryManagmentSystem/view_books.cs
+++ LibraryManagmentSystem/view_books.cs
@@ -12,6 +12,7 @@ namespace LibraryManagmentSystem
 {
     public partial class view_books : Form
     {
+        int res = 0;
         SqlConnection conn = new SqlConnection("Data Source=DESKTOP-S5A1R3M;Initial Catalog=LMS;Integrated Security=True");
         public view_books()
         {
@@ -36,10 +37,80 @@ namespace LibraryManagmentSystem
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 da.Fill(dt);
                 dataGridView1.DataSource = dt;
+                conn.Close();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select * from books_info where book_name  like('%" + textBox1.Text + "%')  or book_author like('%" + textBox1.Text + "%')";
+                    cmd.ExecuteNonQuery();
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    res = Convert.ToInt32(dt.Rows.Count.ToString());
+                    dataGridView1.DataSource = dt;
+                    conn.Close();
+
+                    if (res == 0)
+                    {
+                        MessageBox.Show("No Record Found");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a book name to search");
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select * from books_info where book_name  like('%" + textBox1.Text + "%') or book_author like('%" + textBox1.Text + "%')";
+                    cmd.ExecuteNonQuery();
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    res = Convert.ToInt32(dt.Rows.Count.ToString());
+                    dataGridView1.DataSource = dt;
+                    conn.Close();
+
+                    if (res == 0)
+                    {
+                        MessageBox.Show("No Record Found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter a book name to search");
             }
         }
     }
