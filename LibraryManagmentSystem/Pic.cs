c51aa0f0698d9421569980e54ff27c919098d109
salmanzafar949 <salmanzafar949@gmail.com>
2017-08-29 23:23:40 +0500

    Create Picture Class and created student table

---
+++ LibraryManagmentSystem/Pic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentSystem
+{
+  public class Pic
+    {
+        public static string GetRandomPassword(int length)
+        {
+            char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            string password = string.Empty;
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int x = random.Next(1, chars.Length);
+                //For avoiding Repetation of Characters
+                if (!password.Contains(chars.GetValue(x).ToString()))
+                    password += chars.GetValue(x);
+                else
+                    i = i - 1;
+            }
+            return password;
+        }
+    }
+}
